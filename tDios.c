2231f3ef4bbe4ff7454337fa70f49084e1c451ce
ikergecex <i.gancedo.2025@alumnos.urjc.es>
2026-05-16 18:53:28 +0200

    Add deity creation and display functions
    Implemented functions to create and display a deity.

--- tDios.c
+++ tDios.c
@@ -1,9 +1,21 @@
+#include <stdio.h>
 #include <string.h>
 #include "tDios.h"
 
 void crearDios(tDios* dios, char nombre[20], char descripcion[80]) {
+    strcpy(dios->nombre, nombre);
+    strcpy(dios->descripcion, descripcion);
+}
+
+void mostrarDios(const tDios *d) {
+    printf("%s\n", d->nombre);
 }
 
 int esIgualDios(char* d1, char* d2) {
     return (strcmp(d1, d2) == 0);
+}
+
+void asignarDios(tDios *d1, tDios d2) {
+    strcpy(d1->nombre, d2.nombre);
+    strcpy(d1->descripcion, d2.descripcion);
 }
